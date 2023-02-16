07f3618f5729af45058e54c9bae4508484657944
Yauhen Klimovich <webrubin1987@gmail.com>
2023-02-16 14:59:10 +0300

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
+    // В конструкторе устанавливаем начальные значения для минимального значения и максимального и текущее
+    // угадываемое число this.currentGuess
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.currentGuess = null;
+    }
+// устанавливает диапазон  возможных чисел для игры
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
+// возвращает текущее угадываемое число
     guess() {
-
+        this.currentGuess = Math.ceil((this.min + this.max) / 2);
+        return this.currentGuess;
     }
-
+// уменьшает максимальное значение this.max до текущего угадываемого числа
     lower() {
-
+        this.max = this.currentGuess;
     }
-
+// увеличивает минимальное значение this.min до текущего угадываемого числа
     greater() {
-
+        this.min = this.currentGuess;
     }
 }
 
